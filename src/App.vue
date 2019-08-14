f816ca874724859196e66a6b04b24e3a8261c94a
Kiyotaka <t10kiyotaka@gmail.com>
2019-08-14 12:32:28 +0900

    Make VIEW

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="header">
+      <h1>Chat</h1>
+      <!-- Show form and logout button if logged in -->
+      <div v-if="user.uid" key="login">
+        [{{ user.displayName }}]
+        <button type="button" @click="doLogout">Logout</button>
+      </div>
+      <!-- Show login button if not logged in -->
+      <div v-else key="logout">
+        <button type="button" @click="doLogin">Login</button>
+      </div>
+    </header>
+
+    <!-- Show list which retrieve from Firebase -->
+    <transition-group name="chat" tag="div" class="list content">
+      <section v-for="{ key, name, image, message} in chat" :key="key" class="item">
+        <div class="item-image"><img :src="image" width="40" height="40"></div>
+        <div class="item-detail">
+          <div class="item-">{{ name }}</div>
+          <div class="item-message">
+            <nl2br tag="div" :v-text="message" />
+          </div>
+        </div>
+      </section>
+    </transition-group>
+
+    <!-- Input form -->
+    <form action="" @submit.prevent="doSend" class="form">
+      <textarea
+        v-model="input"
+        :disabled="!user.uid"
+        @keydown.enter.exact.prevent="doSend"></textarea>
+      <button type="submit" :disabled="!user.uid" class="send-button">Send</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import firebase from 'firebase'
+import Nl2br from 'vue-nl2br'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Nl2br
+  },
+  data() {
+    return {
+      user: {}, // User information
+      chat: [], // Array for retreived messages
+      input: '' // Message
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBhTRJyTi2aMFtI9U8x5fjFVQdijUKa6Ow",
+  authDomain: "vue-chat-e481c.firebaseapp.com",
+  databaseURL: "https://vue-chat-e481c.firebaseio.com",
+  projectId: "vue-chat-e481c",
+  storageBucket: "vue-chat-e481c.appspot.com",
+  messagingSenderId: "140060465249",
+  appId: "1:140060465249:web:54e7203d56f840c9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   render: h => h(App),
