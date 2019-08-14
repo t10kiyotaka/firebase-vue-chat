dae765dac6f939f043f90b8f6eac96ef93660a60
Kiyotaka <t10kiyotaka@gmail.com>
2019-08-14 12:56:30 +0900

    Make script

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,8 @@
         <div class="item-detail">
           <div class="item-">{{ name }}</div>
           <div class="item-message">
-            <nl2br tag="div" :v-text="message" />
+            <!-- <nl2br tag="div" :v-text="message" /> -->
+            <div :v-text="message"></div>
           </div>
         </div>
       </section>
@@ -39,17 +40,72 @@
 
 <script>
 import firebase from 'firebase'
-import Nl2br from 'vue-nl2br'
+// import Nl2br from 'vue-nl2br'
 
 export default {
   components: {
-    Nl2br
+    // Nl2br
   },
   data() {
     return {
       user: {}, // User information
       chat: [], // Array for retreived messages
       input: '' // Message
+    }
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.user = user ? user : {}
+      const ref_message = firebase.database().ref('message')
+      if (user) {
+        this.chat = []
+        // Register handler when message changed
+        ref_message.limitToLast(10).on('child_added', this.childAdded)
+      } else {
+        // Unregister handler when message changed
+        ref_message.limitToLast(10).off('child_added', this.childAdded)
+      }
+    })
+  },
+  methods: {
+    // Login process
+    doLogin() {
+      const provider = new firebase.auth.TwitterAuthProvider()
+      firebase.auth().signInWithPopup(provider)
+    },
+    // Logout process
+    doLogout() {
+      firebase.auth().signOut()
+    },
+    // Move the scroll position to the bottom
+    scrollBottom() {
+      this.$nextTick(() => {
+        window.scrollTo(0, document.body.clientHeight)
+      })
+    },
+    // Add recieved messages to Chat
+    // Called every time when new item is added to DB 
+    childAdded(snap) {
+      const message = snap.val()
+      this.chat.push({
+        key: snap.key,
+        name: message.name,
+        image: message.image,
+        message: message.message
+      })
+      this.scrollBottom()
+    },
+    doSend() {
+      if (this.user.uid && this.input.length) {
+        // Add message to firebase
+        firebase.database().ref('message').push({
+          message: this.input,
+          name: this.user.displayName,
+          image: this.user.photoURL
+        }, () => {
+          this.input = '' // Make form empty
+        })
+      }
     }
   }
 }
